f4623579e887d9c66b0d9c3ea31b6204ebb342ea
Mateus Guimarães Moraes Vilela <mateusguimaraesmoraes14@gmail.com>
2025-09-19 16:31:56 -0400

    Update lista-alunos.php

--- lista-alunos.php
+++ lista-alunos.php
@@ -7,6 +7,6 @@ require 'vendor/autoload.php';
 $databasePath = __DIR__ . 'banco.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath); // Novo objeto da classe PDO
 
-$statement = $pdo->query('SELECT * FROM students'); //retorna um array de objetos da classe PDOStatement
+$statement = $pdo->query('SELECT * FROM students'); //retorna um objeto da classe PDOStatement
 
-var_dump($statement->fetchAll(PDO::FETCH_CLASS), Student::class);
+var_dump($statement->fetchAll(PDO::FETCH_ASSOC));

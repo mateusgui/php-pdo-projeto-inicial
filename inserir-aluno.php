0f3a075074f865a747a3e15f44c664d12341b751
Mateus Guimarães Moraes Vilela <mateusguimaraesmoraes14@gmail.com>
2025-09-22 11:16:10 -0400

    Evitando SQL Injection

--- inserir-aluno.php
+++ inserir-aluno.php
@@ -7,8 +7,22 @@ require 'vendor/autoload.php';
 $databasePath = __DIR__ . 'banco.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
 
-$student = new Student(null, 'Mateus Guimarães', new DateTimeImmutable('2000-05-13'));
+$student = new Student(
+    null,
+    'Stela Marisco Duarte',
+    new DateTimeImmutable('2000-12-30')
+);
 
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}');";
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES(?, ?);";
+$statement = $pdo->prepare($sqlInsert);
 
+$statement->bindValue(1, $student->name());
+$statement->bindValue(2, $student->birthDate()->format('Y-m-d'));
+
+$resultado = $statement->execute();
+var_dump($resultado);
+
+//$sqlInsert = "INSERT INTO students (name, birth_date) VALUES('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}');";
+
+exit();
 var_dump($pdo->exec($sqlInsert));
--- lista-alunos.php
+++ lista-alunos.php
@@ -9,4 +9,11 @@ $pdo = new PDO('sqlite:' . $databasePath); // Novo objeto da classe PDO
 
 $statement = $pdo->query('SELECT * FROM students'); //retorna um objeto da classe PDOStatement
 
-var_dump($statement->fetchAll(PDO::FETCH_ASSOC));
+while($studentData = $statement->fetch(PDO::FETCH_ASSOC)){ //Vai permanecer true enquanto existirem registros sendo retornados
+    $student = new Student($studentData['id'], $studentData['name'], new DateTimeImmutable($studentData['birth_date']));
+
+    echo $student->name() . "\n";
+}
+
+
+//var_dump($listaDeAlunos);
